de93611bd8ff8ba3098a8f4d8cc0837d6e7f6711
Diegomau182 <Diegomauri2013@gmail.com>
2022-02-23 15:10:15 -0600

    Creacion de las carpetas

---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, Text, NativeEventEmitter } from "react-native";
+const Home = ({ navigation }) => {
+    return ();
+}
+const styles = StyleSheet.create({
+
+});
+
+export default Home
