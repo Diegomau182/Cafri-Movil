b62d5bddbbd537f5b5a2dd5dfc6ca5cf7adbf93a
Diegomau182 <Diegomauri2013@gmail.com>
2022-02-23 23:30:26 -0600

    Diseño y creacion de la navegacion de la pantalla de inicio (boton de frijol y logo de la aplicacion)

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,55 @@
 import React from "react";
-import { StyleSheet, View, Text, NativeEventEmitter } from "react-native";
+import { StyleSheet, View,TouchableOpacity ,Text,Image,Button} from "react-native";
 const Home = ({ navigation }) => {
-    return ();
+    return (
+        < >
+            <View style={styles.fondo}>
+                <View style={styles.contenedorImagen}>
+                    <Image style={styles.estiloImagen} source={require('../../assets/imagenes/CafriLogo.png')}/>
+                </View>
+                <View style={styles.contenedorBotones}>
+                    <TouchableOpacity style={styles.fondoBotones}>
+                        <Image style={styles.tamañoIconos} source={require("../../assets/imagenes/Icono-Frijol.png")}/>
+                        <Text>Frijol</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </>
+    );
 }
 const styles = StyleSheet.create({
+    contenedorImagen: {
+        flex: 1,
+        marginBottom:10,
+        alignItems:"center"
+      },
+    contenedorBotones: {
+        flex:2,
+        alignItems:"center",
+        marginTop:"10%"
+    },
+    estiloImagen:{
+        width:"80%",
+        height:"130%"
+    },
+    fondo:{
+       flex:1,
+       backgroundColor:'#F1F1F5',
+    },
+    fondoBotones:{
+        alignItems:"center",
+        flexDirection:"row",
+        backgroundColor:'#F38F1D',
+        width:"70%",
+        height:"18%",
+        borderRadius:30,
+    },
+    tamañoIconos:{
+       width:"20%",
+       height:"80%",
+       marginLeft:"5%",
+       marginRight:"20%",        
+    }
 
 });
 
