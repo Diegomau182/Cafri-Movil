5a7701d608bd3bc3d5b8756f90950af185349fba
Diegomau182 <Diegomauri2013@gmail.com>
2022-03-03 11:39:34 -0600

    Se agrego la pantalla de caja de herramienta de cafe con la navegacion a la pantalla de home de cafe

--- src/screens/pantallasCafe/pantallaCajaHerramientasCafe.js
+++ src/screens/pantallasCafe/pantallaCajaHerramientasCafe.js
@@ -1,12 +1,43 @@
 import React from "react"
-import {Text,View} from "react-native"
+import {Text,View,StyleSheet,Image,TouchableOpacity} from "react-native"
 
 const pantallaCajaHerramientasCafe = ({navigation}) =>{
     return(
+        <View style={styles.fondo}>
+        <View style={styles.contenedorNavegacion}>
+            <TouchableOpacity style={styles.flecha} onPress={()=>{navigation.navigate("homeTabCafe")}}>
+                    <Image style={styles.tamañoFlecha} source={require('../../../assets/imagenes/flecha.png')}/>
+            </TouchableOpacity>
+        </View>
         <View>
-            <Text>hola soy caja de herramientas cafe</Text>
+            <Text>soy la pantalla de caja de herramientas cafe</Text>
+        </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    fondo:{
+        marginTop:"6%",
+        flex:1,
+        backgroundColor:'#F1F1F5',
+     },
+     contenedorNavegacion: {
+        marginTop:"1%",
+        backgroundColor:"#9FA617",
+        alignItems:"flex-start",
+        flexDirection:"row",
+        height:"10%"
+      },
+    tamañoFlecha:{
+        marginLeft:"2%",
+        width:"90%",
+        height:"100%"
+    },
+    flecha:{
+        marginLeft:"1%",
+        width:"20%"
+    }
+
+})
 
 export default pantallaCajaHerramientasCafe
