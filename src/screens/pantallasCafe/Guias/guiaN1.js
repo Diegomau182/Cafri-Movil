93ed46c747201dce328cf88e5118fdd36e44870d
dleann <dinaleanng@gmail.com>
2022-03-10 19:45:55 -0600

    Creacion de Pantalla Info Texto

--- src/screens/pantallasCafe/Guias/guiaN1.js
+++ src/screens/pantallasCafe/Guias/guiaN1.js
@@ -53,9 +53,15 @@ const guiaN1 = ({navigation}) =>{
           <Image style={styles.pdf} source={require('../../../../assets/imagenes/pdf.png')}/>
           
         </View>
-        <View style={styles.row}>
-            <View></View>
-            <ComponenteListaGuias titulo="Presentación"  numero="01" callback={()=>{navigation.navigate("pantallaInfoTI",{title:"hola soy presentacion"})}} />
+        <View style={styles.row}>            
+            <ComponenteListaGuias titulo="Presentación"  numero="01" 
+            callback={()=>{navigation.navigate(
+              "pantallaInfoT",{title:"Presentación TECHNOSERVE",info:
+              `TechnoServe Inc., es una organización internacional que trabaja con personas emprendedoras del mundo en desarrollo para crear fincas, negocios y sectores económicos más competitivos. Con más de 16 años de presencia en Honduras, TechnoServe ha ejecutado proyectos para desarrollo de los sectores agrícolas, agroindustrial, agromercados y emprendedurismo que han impactado la vida de más de 53,500 personas en todo el país al 2018.
+              
+Actualmente ejecuta la segunda fase del proyecto Mejoramiento Agrícola Sostenible 2.0 – (MAS 2.0), financiado por el Departamento de Agricultura de los Estados Unidos (USDA). El Proyecto MAS 2.0 trabaja con 22,000 productores de café. El área de influencia se ubica en los departamentos de Yoro, Comayagua, Olancho, Francisco Morazán, El Paraíso, La Paz, Santa Bárbara, Intibucá y Cortés. Uno de los principales objetivos del proyecto es mejorar e incrementar la productividad y competitividad del sector café en Honduras, preservando la calidad del producto.
+              
+MAS 2.0 pretende incrementar la productividad mediante asistencia técnica, uso apropiado de tecnologías, adaptación de mejores prácticas agronómicas, vinculación a productores con mercados formales, permitiéndoles tener cultivos sostenibles y un incremento considerable en sus ingresos.`})}} />
             <View style = {styles.lineStyle}/>
             <ComponenteListaGuias titulo="¿Qué es café de calidad?"  numero="02" callback={()=>{navigation.navigate("pantallaInfoTI",{title:"caja de herramientas"})}}/>
             <View style = {styles.lineStyle}/>
